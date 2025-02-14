fc0f1756f9e8d65de93d5fd22f008054f67833b8
augusto.vaz <augusto.vaz@luizalabs.com>
2025-02-14 18:34:01 -0300

    chore: cleanup project

--- custom-mimir/run.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-    --name custom-mimir \
-    -p 8080:8080 \
-    -v ./mimir.yaml:/etc/mimir.yaml \
-    -v ./alertmanager.yaml:/etc/alertmanager-fallback-config.yaml \
-    augustodsgv/custom-mimir
